0301d2630f96828d3a29320ac9c2f519f6397370
Hüseyin Özlü <59911573+hus3y1n@users.noreply.github.com>
2021-06-21 22:17:17 +0300

    Last Commit

---
+++ northwind/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,37 @@
+package kodlamaio.northwind.dataAccess.abstracts;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import kodlamaio.northwind.entities.concrates.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
+
+public interface ProductDao extends JpaRepository<Product,Integer> {
+	Product getByProductName(String productName);
+	
+	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
+	
+	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
+	
+	List<Product> getByCategoryIn(List<Integer> categories);
+	
+	List<Product> getByProductNameContains(String productName); //ürün ismine göre
+	// arar.
+	
+	List<Product> getByProductNameStartsWith(String productName); 
+	
+	@Query("From Product where productName=:productName and category.categoryId=:categoryId")// Veri tabanı tablosunu unut
+	List<Product> GetByNameAndCategory(String productName, int categoryId);
+/*-*-*/
+	@Query("Select new kodlamaio.northwind.entities.dtos.ProductWithCategoryDto"
+			+ "(p.id, p.productName, c.categoryName) "
+			+ " From Category c Inner Join c.products p")
+	List<ProductWithCategoryDto> getProductWithCategoryDetails();
+	
+	//JPQL 
+   /* select p.productId, p.productName from Category c inner join Product p
+	* on c.categoryId = p.categoryId
+	*/
+}
